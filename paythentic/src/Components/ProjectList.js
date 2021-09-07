b7a2071701f6f79dd80fa45c809804871ba6146e
Md Irfan <66679282+mdirfan-code@users.noreply.github.com>
2021-09-07 18:14:54 +0530

    Added heading to page 'project list', added styling to heading & its wrapper, and added positioning style to project buttons ('Active' and 'History')

--- paythentic/src/Components/ProjectList.js
+++ paythentic/src/Components/ProjectList.js
@@ -3,7 +3,7 @@ import './ProjectList.css'
 export default function ProjectList() {
     return (
         <div className="bg-DshBrd">
-            <h1>Your Projects</h1>
+            <span className="title-wrap"><h1 className='title-your-proj'>Your Projects</h1></span>
             <span className="proj-btns"><button className="proj-btn active-proj">Active</button><button className="proj-btn history-proj">History</button></span>
            
         </div>
