a080d29a549f1a5c6a5ff7827311e7d169c6272f
Md Irfan <66679282+mdirfan-code@users.noreply.github.com>
2021-09-11 14:34:04 +0530

    fixed navbar's profile option menu

--- paythentic/src/Components/NavBar.js
+++ paythentic/src/Components/NavBar.js
@@ -2,30 +2,42 @@ import React,{useState} from 'react';
 import './NavBar.css';
 import Icon from '@mdi/react'
 import { mdiMagnify } from '@mdi/js'
+import { mdiClose } from '@mdi/js';
+
 
 function NavBar({userName}) {
     const [ isDialBoxVisible, setVisibility] = useState(false)
-   
-    const clickHandler = (Visible) => {
-       
-        setVisibility(Visible)
+ 
 
-    }
-    console.log(isDialBoxVisible)
    
     return (
         <nav className="NavBar">
             <a id="paythentic-logo"><h1>PayThentic</h1></a>
             <div className="right-cor"><span id='search-bar'><Icon path={mdiMagnify} title="User Profile" size={1}   color="rgb(6, 27, 41)" onMouseOver/><input  type="text" placeholder="Search"/></span>
-            <div className="profile-option" onMouseOver={() => clickHandler(true)} ><h2>@{userName}</h2><img className="profile-image" src="https://source.unsplash.com/EQFtEzJGERg/1600x1600" alt="Profile Pic"/></div>
-            <div className={`Options-${isDialBoxVisible?"OnOption":"OffOption"}`} onMouseOut={() => clickHandler(false) }>
-                <span>Update Profile</span>
-                <button>Log Out</button>
-                </div></div>
+            <div className="profile-option" onClick={() => setVisibility(!isDialBoxVisible) } >
+                <h2>@{userName}</h2>
+                <img className="profile-image" src="https://source.unsplash.com/EQFtEzJGERg/1600x1600" alt="Profile Pic"/>
+                </div>
+                {isDialBoxVisible && <Options />}
+                
+            </div>
             
         </nav>
     )
+
+    function Options() {
+        return(
+            <div className={`Options`} >
+                    <span className="cross-btn" onClick={() => setVisibility(false) }><Icon path={mdiClose} title="User Profile" size={1}   color="white" onMouseOver/></span>                
+                    <span className='update-profile-btn'>Update Profile</span>
+                    <button>Log Out</button>
+            </div>
+                 
+        )
+    }
 }
+
+
 
 
 
